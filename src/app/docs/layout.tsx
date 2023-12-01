2ee368c69f09e016b9504a07599788e7389c75e2
Tomas Carnecky <tomc@caurea.org>
2023-12-01 20:03:07 +0100

    .

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,12 +1,28 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Page } from "timvir/core";
 import "timvir/styles.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const asPath = usePathname();
+
   return (
-    <Page toc={[]} location={{ asPath: "", push() {} }} Link={Link}>
+    <Page
+      toc={[
+        {
+          label: "Home",
+          path: "/docs",
+        },
+        {
+          label: "Components",
+          path: "/docs/components",
+        },
+      ]}
+      location={{ asPath, push() {} }}
+      Link={Link}
+    >
       {children}
     </Page>
   );
