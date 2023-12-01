452cad0c7691eec3d25123dfa0064747358ce82e
Tomas Carnecky <tomc@caurea.org>
2023-12-01 20:11:06 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { remarkPlugin } from "@timvir/mdx";
 
-module.exports = nextConfig
+export default {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: "@mdx-js/loader",
+          options: {
+            providerImportSource: "@mdx-js/react",
+            remarkPlugins: [remarkPlugin],
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
--- src/app/docs/components/[component]/page.tsx
+++ src/app/docs/components/[component]/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import Body from "../../../../components/Foo/docs/index.mdx";
+
 export default function Page(props: any) {
-  return <main>Component {props.params.component}</main>;
+  return (
+    <main>
+      Component {props.params.component}
+      <Body />
+    </main>
+  );
 }
