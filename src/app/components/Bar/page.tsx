63e2ae6c697bc05c6d3673955a23c6b942fc34c8
Tomas Carnecky <tomc@caurea.org>
2023-12-01 20:37:57 +0100

    .

--- src/app/components/Bar/page.tsx
+++ src/app/components/Bar/page.tsx
@@ -1,4 +1,4 @@
 "use client";
 
-import Content from "../../../../components/Bar/docs/index.mdx";
+import Content from "../../../components/Bar/docs/index.mdx";
 export default () => <Content />
--- src/app/components/Foo/page.tsx
+++ src/app/components/Foo/page.tsx
@@ -1,4 +1,4 @@
 "use client";
 
-import Content from "../../../../components/Foo/docs/index.mdx";
+import Content from "../../../components/Foo/docs/index.mdx";
 export default () => <Content />
